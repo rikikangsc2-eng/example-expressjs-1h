cd9b021b64c6bba289722360e616458264ae488b
rikikangsc2-eng <nueapi11@gmail.com>
2024-09-08 19:22:14 +0700

    Update RickyPurPur.js

--- RickyPurPur.js
+++ RickyPurPur.js
@@ -184,16 +184,8 @@ module.exports = sansekai = async (client, m, chatUpdate) => {
             if (m.quoted.text.includes("> mau *.nyerah* apa *.hint*") && !isCmd2) {
                 const topSkor = await gameku.jawabSoal(nomorUser, m.body);
                 const userMentions = tagUser(topSkor);
-                const response = await axios.get('https://nue-api.vercel.app/api/lgpt', {
-                                params: {
-                                    user: m.chat, 
-                                    text: "aku sudah menjawab",
-                                    systemPrompt: 'anda adalah alicia AI',
-                                    aiMessage: `Pengguna menjawab soal dalam game "${m.quoted.text}", dan jawabannya adalah "${m.body}". Beri tau pengguna bahwa jawabannya adalah "${topSkor}".`
-                                }
-                });
                 return client.sendMessage(m.chat, {
-                    text: response.data.result,
+                    text: topSkor,
                     mentions: userMentions
                 }, {
                     quoted: m
@@ -242,10 +234,7 @@ module.exports = sansekai = async (client, m, chatUpdate) => {
             }
         }
         //Auto Ai
-        const allMenus = [...arrMenuDownloader, ...arrMenuAI, ...arrMenuAnime, ...arrMenuTools, ...arrMenuFun, ...arrMenuMaker, ...arrMenuOther];
-const sortedMenus = allMenus.sort();
-const formattedMenus = sortedMenus.map(menu => `/${menu}`);
-        const prompt = `Anda adalah Alicia AI, AI model terbaru yang paling canggih, Anda mempunyai sikap seperti remaja perempuan. Anda mempunyai fitur unggulan seperti:\n${formattedMenus}\nAnda tidak boleh membuat fitur sendiri, Anda hanya boleh memberikan fitur pada list yang tertera saja. Anda berinteraksi layaknya anak remaja.${m.quoted ? `\n\nPengguna membalas pesan Anda yang ini: "${m.quoted.text || ''}"` : ''}. Note : Anda hanya program berbasis text dan tidak dapat menjalankan cmd nya, anda hanya dapat menyuruh pengguna untuk melakukanya secara manual.`;
+const prompt = `Anda adalah alicia AI ai yang cuek anda tidak suka berinteraksi`;
 const autoAI = async () => {
             try {
 
